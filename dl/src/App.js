21556ee6813e1b1f8e6bafb74965cee1c2e1e2b6
kaustuvkaran01 <kaustuvkaran01@gmail.com>
2020-01-15 01:02:05 +0530

    Template for the Docs page ready

---
+++ dl/src/App.js
@@ -0,0 +1,14 @@
+import React ,{Component} from 'react';
+import 'tachyons';
+import './App.css';
+import Docs from './components/Docs/Docs'
+
+class App extends Component {
+  render() {
+    return(
+      <Docs />
+    )
+  }
+}
+
+export default App;
