17db1114a5e7e92b40fea777101a07e032da65c0
kaustuvkaran01 <kaustuvkaran01@gmail.com>
2020-01-15 18:04:44 +0530

    Just some minor changes

--- dl/src/App.js
+++ dl/src/App.js
@@ -2,7 +2,7 @@ import React ,{Component} from 'react';
 import 'tachyons';
 import './App.css';
 import './components/fonts.css';
-import Docs from './components/Docs/Docs'
+import Docs from './components/Docs/Docs.jsx'
 
 class App extends Component {
   render() {
