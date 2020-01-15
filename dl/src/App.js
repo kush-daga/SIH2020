05a87c42ea099711fc854e1facb8b89874c9cf1c
kaustuvkaran01 <kaustuvkaran01@gmail.com>
2020-01-16 00:57:17 +0530

    Something

--- dl/src/App.js
+++ dl/src/App.js
@@ -2,12 +2,14 @@ import React ,{Component} from 'react';
 import 'tachyons';
 import './App.css';
 import './components/fonts.css';
-import Docs from './components/Docs/Docs.jsx'
+import Docs from './components/Docs/Docs.jsx';
+import Signin from './components/Signin/Signin';
 
 class App extends Component {
   render() {
     return(
-      <Docs />
+      // <Docs />
+      <Signin />
     )
   }
 }
