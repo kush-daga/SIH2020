faf3824febb0ef8d7fc76c83119411e9ef5c3609
Kush Daga <kushdaga@hotmail.com>
2020-01-15 23:08:41 +0530

    changes

--- dl/src/components/Docs/Docs.jsx
+++ dl/src/components/Docs/Docs.jsx
@@ -1,117 +1,128 @@
-import React from 'react';
-import './Docs.css';
-import 'tachyons';
+import React from "react";
+import "./Docs.css";
+import "tachyons";
+import styled from "styled-components";
 const Docs = () => {
-    return(
-        <div className='container'>
-            <div className='left'>
-                <p style={{fontWeight:'bold'}}>docLedger</p>
-                <p style={{fontSize:'20px',color:'#FFC1C1'}}>"Your health records, our security"</p>
+    return (
+        <Container>
+            <div className="left">
+                <p style={{ fontWeight: "bold" }}>docLedger</p>
+                <p style={{ fontSize: "20px", color: "#FFC1C1" }}>
+                    "Your health records, our security"
+                </p>
             </div>
 
-            <div className='right'>
-                <div className='Heading'>
-                    <p><b>What exactly is docLedger?</b></p>
-                </div>
-                <div className='Data'>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
-                    </p>
-                    <p>This is what you get when you do something insane..... 
-                    <br /> Something amazing
+            <div className="right">
+                <div className="Heading">
+                    <p>
+                        <b>What exactly is docLedger?</b>
                     </p>
                 </div>
-                
-
+                <div className="Data">
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                    <p>
+                        This is what you get when you do something insane.....
+                        <br /> Something amazing
+                    </p>
+                </div>
             </div>
-        </div>
-    )
-}
+        </Container>
+    );
+};
 export default Docs;
+
+const Container = styled.div`
+    display: flex;
+    height: 100vh;
+`;
