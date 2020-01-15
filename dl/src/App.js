ed3d0d6d209298a111541ee74cf59ecb6c29555b
kaustuvkaran01 <kaustuvkaran01@gmail.com>
2020-01-15 17:18:00 +0530

    Fonts and a font.css file added

--- dl/src/App.js
+++ dl/src/App.js
@@ -1,6 +1,7 @@
 import React ,{Component} from 'react';
 import 'tachyons';
 import './App.css';
+import './components/fonts.css';
 import Docs from './components/Docs/Docs'
 
 class App extends Component {
